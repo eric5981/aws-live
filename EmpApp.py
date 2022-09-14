b93dedc2365572f67cee7fbb3d3fd1de04bfa2b9
chuahjx-wp19@student.tarc.edu.my <chuahjx-wp19@student.tarc.edu.my>
2022-09-14 17:15:45 +0800

    att

--- EmpApp.py
+++ EmpApp.py
@@ -106,8 +106,13 @@ def FetchInfo():
 
         (id, fname, lname, priskill, location, salary) = emp[0]
         image_url = show_image(custombucket)
+        att_emp_sql = "SELECT attendance.datetime, attendance.status FROM attendance INNER JOIN employee ON attendance.emp_id = employee.emp_id WHERE employee.emp_id = %s"
+        mycursor = db_conn.cursor()
+        mycursor.execute(att_emp_sql, (emp_id))
+        att_result= mycursor.fetchall()
+        (dt,status) = att_result[-1]
         #return render_template('GetEmpOutput.html',id=id,fname=fname,lname=lname,skill=priskill,location=location,salary=salary,image_url=image_url)
-        return render_template('GetEmployeeOutput.html',id=id,fname=fname,lname=lname,skill=priskill,location=location,salary=salary,image_url=image_url)
+        return render_template('GetEmployeeOutput.html',id=id,fname=fname,lname=lname,skill=priskill,location=location,salary=salary,image_url=image_url,dt=dt,status=status)
     except Exception as e:
         return str(e)
 
