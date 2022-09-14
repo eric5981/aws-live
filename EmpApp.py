5e42225e0b07146fdd4598012a113e01ce8e7348
chuahjx-wp19@student.tarc.edu.my <chuahjx-wp19@student.tarc.edu.my>
2022-09-14 17:32:03 +0800

    att

--- EmpApp.py
+++ EmpApp.py
@@ -111,7 +111,7 @@ def FetchInfo():
         att_result = mycursor.fetchall()
         (dt,status) = att_result[0]
         #return render_template('GetEmpOutput.html',id=id,fname=fname,lname=lname,skill=priskill,location=location,salary=salary,image_url=image_url)
-        return render_template('GetEmployeeOutput.html',id=id,fname=fname,lname=lname,skill=priskill,location=location,salary=salary,image_url=image_url)
+        return render_template('GetEmployeeOutput.html',id=id,fname=fname,lname=lname,skill=priskill,location=location,salary=salary,image_url=image_url,status=status)
     except Exception as e:
         return str(e)
 
