9cfd337ac9818b6fd0c4421805900d6c46b1d046
eric5981 <ericwts-wm20@student.tarc.edu.my>
2022-09-15 01:56:12 +0800

    .

--- EmpApp.py
+++ EmpApp.py
@@ -209,8 +209,10 @@ def PayrollUpdate():
    # pri_skill = request.form['pri_skill']
     #location = request.form['location']
 
-    salary = float(request.form['salary'])
-    deduction = float(request.form['deduction'])
+    salary = request.form.get('salary', type=float)
+    #salary = float(request.form['salary'])
+    deduction = request.form.get('deduction', type=float)
+    #deduction = float(request.form['deduction'])
     #emp_image_file = request.files['emp_image_file']
     update_sql = "UPDATE employee SET salary = %d, deduction = %d WHERE emp_id = %s"
     #update_sql = "UPDATE employee SET first_name = %s, last_name = %s, pri_skill = %s, location = %s WHERE emp_id = %s"
