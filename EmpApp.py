43ebe95e6ee5889d22c72cf568ae7419f234091c
eric5981 <ericwts-wm20@student.tarc.edu.my>
2022-09-15 16:23:25 +0800

    Profile image havne't

--- EmpApp.py
+++ EmpApp.py
@@ -135,7 +135,7 @@ def show_image(bucket,emp_id):
         for item in s3_client.list_objects(Bucket=bucket)['Contents']:
             presigned_url = s3_client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': item['Key']}, ExpiresIn = 100)
             if emp_id in presigned_url:
-                public_urls.append(presigned_url)
+                public_urls.append(presigned_url) #maybe around here de issue ba because print out all image, access key will be print out
     except Exception as e:
        pass
     #print(public_urls)
